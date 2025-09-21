8ba8826823c6b66cd27a905b785dcb2b86e75d01
João Otavio  Mesquita <93503467+Otavio17061992@users.noreply.github.com>
2025-09-21 03:17:35 +0000

    metodos preliminarios de aluno

---
+++ Models/Entities/Metodos/AlunoMetodos.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EduManager.Models.Entities.Dominios;
+
+namespace EduManager.Models.Entities.Metodos;
+
+public class AlunoMetodos
+{
+    private void InserirAluno(AlunoDominio aluno)
+    {
+
+    }
+
+    private void AtualizarAluno(AlunoDominio aluno)
+    {
+
+    }
+
+    private void DeletarAluno(int alunoId)
+    {
+
+    }
+
+    private AlunoDominio? ListarAlunoPorNome(string nome)
+    {
+        return null;
+    }
+
+    private List<AlunoDominio?> ListarTodosAlunos()
+    {
+        return new List<AlunoDominio?>();
+    }
+}
